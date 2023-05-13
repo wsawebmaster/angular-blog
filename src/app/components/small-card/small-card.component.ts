d107c673538c6be29db5b27fd4d489c6dbeff510
Wagner Andrade <wsawebmaster@yahoo.com.br>
2023-05-13 18:54:43 -0300

    added dynamic components

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = 'Saiu a nova versão do Angular';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
